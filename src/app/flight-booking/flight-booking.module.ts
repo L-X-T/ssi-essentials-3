09757ae9fab07cbe8f0d391f64ad09faf5ec781a
Alex Thalhammer <alex@angular-software.at>
2021-07-01 09:58:05 +0200

    FlightStatusToggleComponent

--- src/app/flight-booking/flight-booking.module.ts
+++ src/app/flight-booking/flight-booking.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { FlightSearchComponent } from './flight-search/flight-search.component';
 import { FlightCardComponent } from './flight-card/flight-card.component';
+import { FlightStatusToggleComponent } from './flight-status-toggle/flight-status-toggle.component';
 
 @NgModule({
   imports: [SharedModule],
-  declarations: [FlightSearchComponent, FlightCardComponent],
+  declarations: [FlightSearchComponent, FlightCardComponent, FlightStatusToggleComponent],
   exports: [FlightSearchComponent]
 })
 export class FlightBookingModule {}
