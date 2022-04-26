ca7a7423f367cbb1e1bb7d01a091f3805176436c
Alexander Thalhammer <alex@angular-software.at>
2022-04-26 16:44:34 +0200

    Add observable/subscription/async pipe demo

--- src/app/flight-search/flight-search.component.ts
+++ src/app/flight-search/flight-search.component.ts
@@ -1,18 +1,26 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnDestroy } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
 
 import { Flight } from '../entities/flight';
 import { FlightService } from './flight.service';
+import { Observable, Observer, Subject, Subscription } from 'rxjs';
+import { share, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-flight-search',
   templateUrl: './flight-search.component.html',
   styleUrls: ['./flight-search.component.css']
 })
-export class FlightSearchComponent {
+export class FlightSearchComponent implements OnDestroy {
   from = 'Graz';
   to = 'Hamburg';
+
   flights: Flight[] = [];
+  flights$?: Observable<Flight[]>;
+  flightsSubscription?: Subscription;
+  // private readonly onDestroySubject = new Subject<void>();
+  // readonly terminator$ = this.onDestroySubject.asObservable();
+
   selectedFlight?: Flight;
 
   message = '';
@@ -21,17 +29,31 @@ export class FlightSearchComponent {
   // constructor(private flightService: FlightService) {}
 
   onSearch(): void {
-    this.flightService.find(this.from, this.to).subscribe({
-      next: (flights) => {
-        this.flights = flights;
-      },
-      error: (errResp: HttpErrorResponse) => {
-        console.error('Error loading flights', errResp);
-      },
-      complete: () => {
-        console.debug('Flights loading completed.');
-      }
-    });
+    // 1. my observable
+    this.flights$ = this.flightService.find(this.from, this.to).pipe(share());
+
+    // 2. my observer
+    const flightsObserver: Observer<Flight[]> = {
+      next: (flights) => (this.flights = flights),
+      error: (errResp: HttpErrorResponse) => console.error('Error loading flights', errResp),
+      complete: () => console.debug('Flights loading completed.')
+    };
+
+    // 3a. my subscription
+    this.flightsSubscription?.unsubscribe();
+    this.flightsSubscription = this.flights$.subscribe(flightsObserver);
+
+    // 3b. takeUntil terminator$ emits
+    // this.flights$.pipe(takeUntil(this.terminator$)).subscribe(flightsObserver);
+  }
+
+  ngOnDestroy(): void {
+    // 4a. my unsubscribe
+    this.flightsSubscription?.unsubscribe();
+
+    // 4b. subject emit thru terminator$
+    // this.onDestroySubject.next(void 0);
+    // this.onDestroySubject.complete();
   }
 
   onSelect(selectedFlight: Flight): void {
