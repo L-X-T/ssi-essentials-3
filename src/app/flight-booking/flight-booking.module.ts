ad67397bb20190104182da4a657dff020afd7e28
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2023-04-20 10:16:24 +0200

    [T-d forms] Bonus: Asynchronous Validator

--- src/app/flight-booking/flight-booking.module.ts
+++ src/app/flight-booking/flight-booking.module.ts
@@ -6,6 +6,7 @@ import { FlightCardComponent } from './flight-card/flight-card.component';
 import { FlightStatusToggleComponent } from './flight-status-toggle/flight-status-toggle.component';
 import { FlightValidationErrorsComponent } from './flight-validation-errors/flight-validation-errors.component';
 import { CityValidatorDirective } from './shared/validation/city-validator.directive';
+import { AsyncCityValidatorDirective } from './shared/validation/async-city-validator.directive';
 
 @NgModule({
   imports: [SharedModule],
@@ -14,7 +15,8 @@ import { CityValidatorDirective } from './shared/validation/city-validator.direc
     FlightCardComponent,
     FlightStatusToggleComponent,
     FlightValidationErrorsComponent,
-    CityValidatorDirective
+    CityValidatorDirective,
+    AsyncCityValidatorDirective
   ],
   exports: [FlightSearchComponent]
 })
