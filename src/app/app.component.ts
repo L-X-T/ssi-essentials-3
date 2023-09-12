3a5bd5fff32631b51ead9a0c9213e930ad95f84c
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2023-09-12 11:54:34 +0200

    Migrate bootstrapping to AppComponent without AppModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
+import { FlightSearchComponent } from './components/flight-search/flight-search.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @Component({
+  standalone: true,
+  imports: [SidebarComponent, NavbarComponent, FlightSearchComponent],
   selector: 'app-flight-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
