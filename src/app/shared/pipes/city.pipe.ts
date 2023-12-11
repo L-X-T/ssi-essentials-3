L-X-T/fmr

1st Angular Pipe

src/app/shared/pipes/city.pipe.ts
@@ -1,0 +1,34 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'city',
+  pure: true
+})
+export class CityPipe implements PipeTransform {
+  transform(value: string, fmt: string): string {
+    let short, long;
+
+    switch (value) {
+      case 'Graz':
+        short = 'GRZ';
+        long = 'Flughafen Graz Thalerhof';
+        break;
+      case 'Hamburg':
+        short = 'HAM';
+        long = 'Airport Hamburg Fulsbüttel Helmut Schmidt';
+        break;
+      case 'Wien':
+        short = 'VIE';
+        long = 'Flughafen Wien Schwechat';
+        break;
+      default:
+        short = long = value;
+    }
+
+    if (fmt === 'short') {
+      return short;
+    }
+
+    return long;
+  }
+}

L-X-T/ssi-essentials-3

src/app/shared/pipes/city.pipe.ts
@@ -1,0 +1,36 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'city'
+})
+export class CityPipe implements PipeTransform {
+  transform(value: string, format: string): string {
+    let short, long;
+
+    switch (value) {
+      case 'Graz':
+        short = 'GRZ';
+        long = 'Flughafen Graz Thalerhof';
+        break;
+
+      case 'Hamburg':
+        short = 'HAM';
+        long = 'Airport Hamburg Fulsbüttel Helmut Schmidt';
+        break;
+
+      case 'Wien':
+        short = 'VIE';
+        long = 'Flughafen Wien Schwechat';
+        break;
+
+      default:
+        short = long = value;
+    }
+
+    if (format === 'short') {
+      return short;
+    }
+
+    return long;
+  }
+}